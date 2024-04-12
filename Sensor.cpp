0cb9e55138e75131709991a800f63680a7cf95e7
Victor Wang <74549506+VitalzAqua@users.noreply.github.com>
2024-04-11 21:28:49 -0400

    Update Sensor.cpp

--- Sensor.cpp
+++ Sensor.cpp
@@ -3,11 +3,6 @@
 Sensor::Sensor(int electrodeLocationNumber, Waveform waveform) {
     this->electrodeLocation = electrodeLocationNumber;
     this->waveForm = waveForm;
-    // Get the amplitude and frequency values from the waveform
-    for (int i = 0; i < 4; ++i) {
-        amplitudes[i] = waveform.getAmplitude(i);
-        frequencies[i] = waveform.getFrequency(i);
-    }
     // Wait for the EEG to calculate it
     this->dominantFrequency = 0.0;
 }
@@ -16,24 +11,8 @@ int Sensor::getElectrodeLocationNumber() {
     return this->electrodeLocationNumber;
 }
 
-float Sensor::getAmplitute(int num) {
-    return amplitudes[num];
-}
-
-float Sensor::getFrequency(int num) {
-    return frequencies[num;]
-}
-
 double Sensor::getDominantFrequency() {
     return this->dominantFrequency;
-}
-
-void Sensor::setAmplitute(int num, float a) {
-    amplitudes[num] = a;
-}
-
-void Sensor::setFrequency(int num, float f) {
-    frequencies[num] = f;
 }
 
 void Sensor::setDominantFrequency(double domF) {
