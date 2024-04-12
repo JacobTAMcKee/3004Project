d2388b4105bea939af229bebc469d0b79b3880af
Victor Wang <74549506+VitalzAqua@users.noreply.github.com>
2024-04-11 21:28:28 -0400

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -7,17 +7,11 @@ Class Sensor {
     public:
         Sensor(int electrodeLocationNumber, Waveform waveform);
         int getElectrodeLocationNumber();
-        float getAmplitute(int num);
-        float getFrequency(int num);
         double getDominantFrequency();
-        void setAmplitute(int num, float a);
-        void setFrequency(int num, float f);
         void setDominantFrequency(double domF);
 
     private:
         int electrodeLocationNumber;
-        float amplitudes[4];
-        float frequencies[4];
         double dominantFrequency;
         Waveform waveform;
 
