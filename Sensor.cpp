daeb8415a4ab84542cdbdcd57f91137a9a80baf5
Victor Wang <74549506+VitalzAqua@users.noreply.github.com>
2024-04-18 22:21:22 -0400

    Update Sensor.cpp

--- Sensor.cpp
+++ Sensor.cpp
@@ -1,21 +1,16 @@
 #include "Sensor.h"
 
+Sensor::Sensor() {}
+
 Sensor::Sensor(int electrodeLocationNumber, Waveform waveform) :
 waveform(waveform) {
     this->electrodeLocationNumber = electrodeLocationNumber;
     // Wait for the EEG to calculate it
-    dominantFrequency = 0.0;
     YPlotData = new double[3000];
 }
 
-Sensor::Sensor() {}
-
 int Sensor::getElectrodeLocationNumber() {
     return this->electrodeLocationNumber;
-}
-
-double Sensor::getDominantFrequency() {
-    return this->dominantFrequency;
 }
 
 Waveform Sensor::getWaveform() {
@@ -29,6 +24,3 @@ double* Sensor::getYPlotData() {
     return YPlotData;
 }
 
-void Sensor::setDominantFrequency(double domF) {
-    dominantFrequency = domF;
-}
